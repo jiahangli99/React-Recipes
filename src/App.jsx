350561a1821cba06a4da08dad58ade4728eaa8fd
jiahangli99 <liabraham.1999@gmail.com>
2022-04-02 14:12:58 -0600

    added template for nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav'
 import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Nav/>
       <Main/>
     </div>
   );
