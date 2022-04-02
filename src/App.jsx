11dfe207827034ba9917b35410346c591c40a718
jiahangli99 <liabraham.1999@gmail.com>
2022-04-02 14:14:04 -0600

    added template for footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav'
 import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <Nav/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
