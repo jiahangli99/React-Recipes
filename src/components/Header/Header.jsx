6328fb39634a7d37a22beb12f5d672a40a031a0b
jiahangli99 <liabraham.1999@gmail.com>
2022-04-04 11:53:05 -0600

    linked header css and added some styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.css'
 
 function Header(props) {
     return (
         <div className='header'>
-            <h1>Header</h1>
+            <h1 className='title'>Family Recipes</h1>
         </div>
     );
 }
