270a8bea252fa0ae13a7297d67be896526c2968d
jiahangli99 <liabraham.1999@gmail.com>
2022-04-04 12:37:16 -0600

    added template style for main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Main.css'
 
 function Main(props) {
     return (
-        <div>
+        <div className='main'>
             <h1>Main</h1>
         </div>
     );
