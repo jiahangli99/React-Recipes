fb9855d0824c1c0b664ccca4ae7824bf4b40a87b
jiahangli99 <liabraham.1999@gmail.com>
2022-04-12 11:01:02 -0600

    added switch

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Main.css'
 import AllRecipes from '../../pages/AllRecipes'
+import { Switch } from 'react-router-dom';
 
 function Main(props) {
     return (
         <div className='main'>
+            <Switch>
             <AllRecipes />
+            </Switch>
         </div>
     );
 }
