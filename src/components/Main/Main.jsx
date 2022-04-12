6901589064fda44d42ba4935176ab3c02281123f
jiahangli99 <liabraham.1999@gmail.com>
2022-04-12 10:58:27 -0600

    added dependecies

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Main.css'
+import AllRecipes from '../../pages/AllRecipes'
 
 function Main(props) {
     return (
         <div className='main'>
-            <h1>Main</h1>
+            <AllRecipes />
         </div>
     );
 }
