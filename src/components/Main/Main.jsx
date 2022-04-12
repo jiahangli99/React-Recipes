52271e7f7788a92e2fb1b680c320fc45a0e31209
jiahangli99 <liabraham.1999@gmail.com>
2022-04-12 11:03:33 -0600

    imported usestate and useeffect

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import './Main.css'
 import AllRecipes from '../../pages/AllRecipes'
-import { Switch } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 
 function Main(props) {
+    const [notes, setNotes] = useState([])
     return (
         <div className='main'>
             <Switch>
-            <AllRecipes />
+                <Route exact path='/'>
+                    <AllRecipes />
+                </Route>
             </Switch>
         </div>
     );
